66fe0d5e719e3afcdf9f4c0a353b1673ed2865b1
xiaoyu0011 <124509782+xiaoyu0011@users.noreply.github.com>
2024-12-12 09:54:24 +0800

    l2tp.sh

--- l2tp.sh
+++ l2tp.sh
@@ -24,9 +24,8 @@
 # - All values MUST be placed inside 'single quotes'
 # - DO NOT use these special characters within values: \ " '
 
-YOUR_IPSEC_PSK='1'
-YOUR_USERNAME='1'
-YOUR_PASSWORD='1'
+YOUR_USERNAME='123'
+YOUR_PASSWORD='123'
 
 # Important notes:   https://git.io/vpnnotes
 # Setup VPN clients: https://git.io/vpnclients
